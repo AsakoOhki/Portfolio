36f9ee818b3135d0e2cc4618b1db22aa57c2f0bd
Asako Ohki <ohkiasako@gmail.com>
2021-04-25 15:43:22 -0700

    education

---
+++ src/components/Education.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div className="education">
+      <div className="d-flex justify-content-center my-5">
+        <h1>Education</h1>
+      </div>
+      <div className="container education-wrapper">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2019-2020</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda soluta natus tenetur numquam vel pariatur qui quidem odit, nam dicta modi doloribus delectus saepe sunt, quo, officia provident laboriosam consequatur.</p>
+          </div>
+        </div>
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2015-2019</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda soluta natus tenetur numquam vel pariatur qui quidem odit, nam dicta modi doloribus delectus saepe sunt, quo, officia provident laboriosam consequatur.</p>
+          </div>
+        </div>
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2011-2015</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda soluta natus tenetur numquam vel pariatur qui quidem odit, nam dicta modi doloribus delectus saepe sunt, quo, officia provident laboriosam consequatur.</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+
+  )
+}
+
+export default Education
