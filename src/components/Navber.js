82055224344a59fa0d2cab2c48a7382b0a09c484
Asako Ohki <ohkiasako@gmail.com>
2021-04-26 02:45:57 -0700

    Nfixed avbar

--- src/components/Navber.js
+++ src/components/Navber.js
@@ -5,9 +5,9 @@ import { Link } from 'react-scroll';
 const Navber = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
-        <a className="navbar-brand" href="#"></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        </button>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
